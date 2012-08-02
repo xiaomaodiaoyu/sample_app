ecb6307e19b95e318def4235fbddedb773e4ea78
jzh <xiaomaodiaoyu2011@gmail.com>
2012-08-02 03:12:53 -0400

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   # By default, before filters apply to every action in a controller, 
   # so here we restrict the filter to act only on the :edit and :update actions
-  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy] 
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy, :following, :followers] 
   before_filter :correct_user,   only: [:edit, :update]
   before_filter :admin_user,     only: :destroy
 
@@ -52,6 +52,20 @@ class UsersController < ApplicationController
     User.find(params[:id]).destroy
     flash[:success] = "User destroyed."
     redirect_to users_path
+  end
+
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
   end
 
 private
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -31,7 +31,19 @@ describe "Static pages" do
           page.should have_selector("li##{item.id}", text: item.contents)
         end
       end
+
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
+      end
     end
+    
 
   describe "Help page" do
     before { visit help_path }
